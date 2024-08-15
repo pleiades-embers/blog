8f0c07b9ef2a6f39c46cef3d32df3c2fe8fa7b5c
embers <935267851@qq.com>
2024-08-15 23:38:25 +0800

    feat: 对象数组去重优化版

--- basic/js/14-对象数组去重.js
+++ basic/js/14-对象数组去重.js
@@ -1,23 +1,21 @@
+function uniqueArray(arr, key) {
+  const unique= arr.reduce((acc, obj) => {
+    if (!acc.has(obj[key])) {
+      acc.set(obj[key], obj);
+    }
+    return acc;
+  }, new Map());
 
-
-function uniqueArray (arr,key){
-    return arr.reduce((acc,obj)=>{
-        const hasObj=acc.find(item=>item[key]===obj[key])
-        if(!hasObj){
-            acc.push(obj)
-        }
-        return acc
-    },[])
+  return [...unique.values()]
 }
-
 
 // 示例用法
 const array = [
-    { id: 1, name: 'John' },
-    { id: 2, name: 'Jane' },
-    { id: 1, name: 'John' },
-    { id: 3, name: 'Jake' }
-  ];
-  
-const result = uniqueArray(array, 'id');
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+  { id: 1, name: "John" },
+  { id: 3, name: "Jake" },
+];
+
+const result = uniqueArray(array, "id");
 console.log(result);
